9052b2c7d50c3b66a7c909dbd67e30970ff4d6c5
GolfredoPerezFernandez <golfredo.pf@gmail.com>
2024-02-23 22:37:28 -0400

    asd

---
+++ dist/build/q-B38C9UrS.js
@@ -0,0 +1,7 @@
+import{n as r,C as o,q as e,_ as t}from"./q-DXvOZ1vm.js";import{G as a}from"./q--ZznENgb.js";const i=()=>{throw"Symbol removed by Qwik Optimizer, it can not be called from current platform"},_=a(o("s_GQamrjryd1Y")),n=r(e(()=>t(()=>import("./q-C-fNB-bd.js"),__vite__mapDeps([])),"s_VkLNXphUh5s"));export{n as default,i as onGet,_ as useServerTimeLoader};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
